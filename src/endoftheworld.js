d29d1dcb4d48b3ea0692c10141035a43b54eb5ba
andymcgrath <andy@stratalabs.uk>
2019-09-04 16:13:24 +0100

    Working game prototype! :P

--- src/endoftheworld.js
+++ src/endoftheworld.js
@@ -4,54 +4,122 @@ let {
     keyPressed,
     GameLoop,
     Pool,
-    Sprite,
-    SpriteSheet,
+    Sprite
 } = kontra;
 
 let gameSetting = {
+    playerScore: 0,
     maxPlasma: 150,
     plasmaBurnRate: 1,
-    plasmaRefillRate: 0.2,
-    plasmaTtl: 120
-    
+    plasmaRefillRate: 0.5,
+    playerFireRate: 0.1,
+    plasmaTtl: 120,
+    canvasHeight: 800,
+    canvasWidth: 800
 }
 
 init();
 initKeys();
 
+let playerSprite
+
+let asteroidArray = []
+let plasmaArray = []
 let spriteArray = []
 
-let blockPool = Pool({
-    create: Sprite
-})
+for (let i = 0; i < 4; i++) {
+    setTimeout(() => createAsteroid(), Math.random() * 3000)
+}
 
-function generateVectorTowardsEarth() {
-    let dy = Math.random() * 4 - 2
-    if (dy < 0) dy *= -1
-    return dy
+function random(number) {
+    return Math.round(Math.random() * number)
+}
+
+function createAsteroid() {
+    let asteroid = Sprite({
+        type: 'asteroid',
+        alive: true,
+        hit: false,
+        x: random(gameSetting.canvasWidth),
+        y: random(-30),
+        anchor: {x: 0.5, y: 0.5},
+        dx: (Math.random() * 3) - 1.5,
+        dy: Math.random(),
+        ddy: 0.005,
+        radius: random(20) + 10,
+        height: this.radius,
+        width: this.radius,
+        update() {
+            this.advance()
+            if ((this.x - this.radius - 1 < 0) || (this.x + this.radius + 1 > gameSetting.canvasWidth)) {
+                this.alive = false
+                if (this.hit) {
+                    gameSetting.playerScore += Math.round(this.radius * 100)
+                }
+                setTimeout(() => createAsteroid(), Math.random() * 3000)
+            }
+            if (this.collidesWith(playerSprite)) {
+                playerSprite.alive = false
+            }
+            if (this.y + this.radius >= gameSetting.canvasHeight) {
+                playerSprite.alive = false
+            }
+        },
+        render() {
+          this.context.strokeStyle = 'white';
+          this.context.beginPath();  // start drawing a shape
+          this.context.arc(this.x, this.y, this.radius, 0, Math.PI*2);
+          this.context.stroke();     // outline the circle
+        }
+    })
+    asteroidArray.push(asteroid)
 }
 
 let plasma01 = document.getElementById('plasma01')
-function createBlock(x, y, size) {
-    blockPool.get({
+function createPlasma(x, y, size) {
+    let plasma = Sprite({
+        type: 'plasma',
+        alive: true,
         x: x,
         y: y,
         anchor: {x: 0.5, y: 0.5},
         width: size,
         height: size,
+        radius: size / 2,
         image: plasma01,
-        ttl: gameSetting.plasmaTtl
+        ttl: gameSetting.plasmaTtl,
+        update() {
+            this.ttl -= 1
+            if (this.ttl < 1) {
+                this.alive = false
+            }
+            asteroidArray.map((asteroid) => {collide(asteroid, this)})
+            // for (asteroid of asteroidArray) {
+                // if (this.collidesWith(asteroid)) {
+                //     gameSetting.playerScore += 10
+                //     this.alive = false
+                //     if (asteroid.dx > 0) asteroid.dx += asteroid.dy
+                //     if (asteroid.dx < 0) asteroid.dx -= asteroid.dy
+                //     asteroid.dy *= -0.5
+                //     asteroid.hit = true
+                // }
+            // }
+        }
     })
+    plasmaArray.push(plasma)
 }
 
 let playerImg = document.getElementById('playerImg')
 playerImg.onload = function() {
     playerSprite = Sprite({
-        x: Math.random() * 800,
-        y: Math.random() * 800,
+        type: 'player',
+        alive: true,
+        x: gameSetting.canvasWidth / 2,
+        y: gameSetting.canvasHeight / 2,
         anchor: {x: 0.5, y: 0.5},
         dx: 0,
         dy: 0,
+        dt: 0,
         width: 24,
         height: 32,
         radius: 16,
@@ -70,30 +138,42 @@ playerImg.onload = function() {
             if (keyPressed('down')) {
                 this.dy += 0.05
             }
-            this.x += this.dx
-            this.y += this.dy
+            this.advance()
             if (this.x - this.radius < 0) {
                 this.x = this.radius
                 this.dx = 0
             }
-            if (this.x + this.radius > 800) {
-                this.x = 800 - this.radius
+            if (this.x + this.radius > gameSetting.canvasWidth) {
+                this.x = gameSetting.canvasWidth - this.radius
                 this.dx = 0
             }
             if (this.y - this.radius < 0) {
                 this.y = this.radius
                 this.dy = 0
             }
-            if (this.y + this.radius > 800) {
-                this.y = 800 - this.radius
+            if (this.y + this.radius > gameSetting.canvasHeight) {
+                this.y = gameSetting.canvasHeight - this.radius
                 this.dy = 0
             }
-            if (keyPressed('space') && this.plasma >= 1) {
-                createBlock(this.x, this.y - (this.radius * 2), (this.radius * 2))
+            this.dt += 1/60
+
+            if (keyPressed('space') && this.plasma >= 1 && this.dt > gameSetting.playerFireRate) {
+                createPlasma(this.x, this.y - (this.radius * 2), (this.radius * 2))
                 this.plasma -= gameSetting.plasmaBurnRate
+                this.dt = 0
+                
             }
             if (!keyPressed('space') && this.plasma < gameSetting.maxPlasma) {
-                this.plasma += gameSetting.plasmaRefillRate
+                // this.plasma += gameSetting.plasmaRefillRate
+                setTimeout(
+                    () => {
+                        playerSprite.plasma += gameSetting.plasmaRefillRate
+                        if (playerSprite.plasma > gameSetting.maxPlasma) {
+                            playerSprite.plasma = gameSetting.maxPlasma
+                        }
+                    },
+                    Math.random() * 1000
+                )
             }
         }
     })
@@ -120,7 +200,7 @@ function createHud() {
 
     let plasmaTxt = Sprite({
         x: 10,
-        y: 790,
+        y: gameSetting.canvasHeight - 10,
         color: 'white',
         width: this.plasma,
         render() {
@@ -131,17 +211,54 @@ function createHud() {
 
     })
     spriteArray.push(plasmaTxt)
+
+    let playerScore = Sprite({
+        x: gameSetting.canvasWidth - 250,
+        y: gameSetting.canvasHeight - 10,
+        color: 'white',
+        width: this.plasma,
+        render() {
+            this.context.fillStyle = this.color;
+            this.context.font = '15px Arial'
+            this.context.fillText(`SCORE: ${gameSetting.playerScore}`, this.x, this.y)
+        }
+
+    })
+    spriteArray.push(playerScore)
+}
+
+function collide(asteroid, plasma) {
+    let dx = asteroid.x - plasma.x;
+    let dy = asteroid.y - plasma.y;
+    if (Math.sqrt(dx * dx + dy * dy) < asteroid.radius + plasma.radius) {
+        gameSetting.playerScore += 10
+        plasma.alive = false
+        if (asteroid.dx > 0) asteroid.dx += asteroid.dy
+        if (asteroid.dx < 0) asteroid.dx -= asteroid.dy
+        asteroid.dy *= -0.5
+        asteroid.hit = true
+    }
 }
 
 let loop = GameLoop({
     update() {
-        blockPool.update()
-        spriteArray.map((sprite) => {
-            sprite.update()
-        })
+        asteroidArray.map((sprite) => {sprite.update()})
+        asteroidArray = asteroidArray.filter(sprite => sprite.alive)
+        plasmaArray.map((sprite) => {sprite.update()})
+        plasmaArray = plasmaArray.filter(sprite => sprite.alive)
+        spriteArray.map((sprite) => {sprite.update()})
+        // spriteArray = spriteArray.filter((sprite) => {sprite.isAlive()})
+        if (!playerSprite.alive) {
+            loop.stop()
+        }
     },
     render() {
-        blockPool.render()
+        asteroidArray.map((sprite) => {
+            sprite.render()
+        })
+        plasmaArray.map((sprite) => {
+            sprite.render()
+        })
         spriteArray.map((sprite) => {
             sprite.render()
         })
